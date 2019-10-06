f58f0d4dd71673824cd20e392ad9f481889dcedc
wakame-tech <kamata1919@gmail.com>
2019-10-06 13:20:14 +0900

    [Add] Say

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # word-war
 
+<img src="https://img.shields.io/badge/-Go-76E1FE.svg?logo=go&style=flat-square">
+<img src="https://img.shields.io/badge/-Elm-3DBB3D.svg?logo=&style=flat-square">
+<img src="https://img.shields.io/badge/-Javascript-FAEB7F.svg?logo=javascript&style=flat-square">
+<img src="https://img.shields.io/badge/-Firebase-000000.svg?logo=firebase&style=flat-square">
+<img src="https://img.shields.io/badge/-Redis-D82C20.svg?logo=redis&style=flat-square">
+<img src="https://img.shields.io/badge/-gRPC-47848F.svg?logo=&style=flat-square">
+<img src="https://img.shields.io/badge/-Docker-48bcfa.svg?logo=docker&style=flat-square">
+<img src="https://img.shields.io/badge/-Bulma-FCEA0.svg?logo=&style=flat-square">
+
 ## init
 ```
 ./protoc-gen.sh
--- server/interface/rpc/word_war_service.go
+++ server/interface/rpc/word_war_service.go
@@ -3,6 +3,8 @@ package rpc
 import (
 	"context"
 	"errors"
+
+	"github.com/i-pu/word-war/server/domain/entity"
 	pb "github.com/i-pu/word-war/server/interface/rpc/pb"
 	"github.com/i-pu/word-war/server/usecase"
 )
@@ -34,7 +36,8 @@ func (s *wordWarService) Game(in *pb.GameRequest, srv pb.WordWar_GameServer) err
 			return err
 		}
 		// 今は累計メッセージが100を超えたら終了するので
-		if counter.Value > 100 {
+		// ! 10件にしましょう
+		if counter.Value > 10 {
 			// TODO: resultを保存する
 			return nil
 		}
@@ -50,7 +53,20 @@ func (s *wordWarService) Game(in *pb.GameRequest, srv pb.WordWar_GameServer) err
 }
 
 func (s *wordWarService) Say(ctx context.Context, in *pb.SayRequest) (*pb.SayResponse, error) {
-	return nil, errors.New("not implemented")
+	// 発言者にはそのまま返す
+	res := &pb.SayResponse{
+		UserId:  in.GetUserId(),
+		Message: in.GetMessage(),
+	}
+
+	// Redisに(部屋ID固定)
+	err := s.messageUsecase.SendMessage("room1", &entity.Message{UserID: in.GetUserId(), Message: in.GetMessage()})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func (s *wordWarService) Result(ctx context.Context, in *pb.ResultRequest) (*pb.ResultResponse, error) {
